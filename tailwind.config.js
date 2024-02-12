fb1d0e433161eabc74b4249cd4fe5e4d5e73ee03
paras <parasad612@gmail.com>
2024-02-12 19:41:41 +0100

    green colours added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "pgreen-1": "#d2e6b5",
+        "pgreen-2": "#c1db9b",
+        "pgreen-3": "#b1cf86",
+        "pgreen-4": "#a0c172",
+        "pgreen-5": "#8eb15c",
+      }
+    },
   },
   plugins: [],
 }
